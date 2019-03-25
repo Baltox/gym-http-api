2214d29a091926aa85f378931b46602fe425473c
Thomas Costagliola <thomas@francepronet.com>
2019-03-25 13:57:51 +0100

    --

--- binding-js/index.php
+++ binding-js/index.php
@@ -6,51 +6,25 @@
 
   <body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
     <script>
 
     var instanceId;
     var baseUrl = "http://localhost:8000";
+    var env = 'CartPole-v0';
 
     $( document ).ready(function() {
-        $.post(baseUrl+'/v1/envs/', {env_id: 'CartPole-v0'}, onEnvCreated);
-    });
+        initEnv();
 
-    function onEnvCreated(data)
-    {
-        var data = jQuery.parseJSON(data);
+        var data;
 
-        instanceId = data.instance_id;
-
-        $.post(baseUrl+'/v1/envs/'+instanceId+'/monitor/start/', {directory: '/tmp/random-agent-results', force: true, resume: false}, onEnvStarted);
-    }
-
-    function onEnvStarted(data)
-    {
-        $.post(baseUrl+'/v1/envs/'+instanceId+'/reset/', {}, onEnvReseted);
-    }
-
-    function onEnvReseted(data)
-    {
-        var data = null;
-        while(data == null || data.done == false) {
-            var returnAjax = $.ajax({
-                type: "POST",
-                url: baseUrl+'/v1/envs/'+instanceId+'/step/',
-                datatype: "json",
-                data: {action: getRandomInt(2), render: true},
-                async: false
-            });
-
-            data = jQuery.parseJSON(returnAjax.responseText);
-
-            sleep(5);
+        while (data == null || data.done == false) {
+            data = step(getRandomInt(2));
         }
-    }
 
-    function onEnvSteped(data)
-    {
-        //$.post(baseUrl+'/v1/envs/'+instanceId+'/step/', {action: 1, render: true}, onEnvSteped);
-    }
+        console.log('finished');
+
+    });
 
     function sleep(milliseconds) {
         var start = new Date().getTime();
@@ -65,7 +39,48 @@
         return Math.floor(Math.random() * Math.floor(max));
     }
 
+    function initEnv() {
+        createEnv();
+        startMonitor();
+        resetEnv();
+    }
 
+    function createEnv() {
+        createEnvDatas = ajax('POST', '/v1/envs/', {env_id: env});
+
+        instanceId = createEnvDatas.instance_id;
+    }
+
+    function startMonitor() {
+        ajax('POST', '/v1/envs/'+instanceId+'/monitor/start/', {directory: '/tmp/random-agent-results', force: true, resume: false});
+    }
+
+    function resetEnv() {
+        ajax('POST', '/v1/envs/' + instanceId + '/reset/');
+    }
+
+    function step(action) {
+        data = ajax('POST', '/v1/envs/'+instanceId+'/step/', {action: action, render: true});
+
+        return data;
+    }
+
+    function ajax(type, url, data) {
+        returnAjax = $.ajax({
+            type: type,
+            url: baseUrl+url,
+            datatype: "json",
+            data: data,
+            async: false
+        });
+
+        returnDatas = '';
+        if(returnAjax.responseText!= '') {
+            returnDatas = jQuery.parseJSON(returnAjax.responseText);
+        }
+
+        return returnDatas;
+    }
     </script>
     </body>
 </html>
